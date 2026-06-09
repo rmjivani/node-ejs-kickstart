93319e9122b611b7e837c9d28d098f053bc0633c
Bill Newman <william.newman@seattlecolleges.edu>
2026-06-09 19:28:11 +0000

    Adds image support, strips bootswatch

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.set('view engine','ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 //index/home URL
 app.get('/',(req,res)=>{
